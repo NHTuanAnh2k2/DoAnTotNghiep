8ad4f710305e2f52ea761f2046047e8eef9f39e1
Duynvph30146 <duynvph30146@fpt.edu.vn>
2024-03-17 10:20:45 +0700

    duy up thuoc tinh dong

--- src/main/java/com/example/demo/controller/AdminController.java
+++ src/main/java/com/example/demo/controller/AdminController.java
@@ -9,6 +9,7 @@ public class AdminController {
     public String qlhoadon() {
         return "admin/qlhoadon";
     }
+
     @GetMapping("/admin/qlthongke")
     public String admin() {
         return "admin/qlthongke";
@@ -33,22 +34,27 @@ public class AdminController {
     public String addnhanvien() {
         return "/admin/addnhanvien";
     }
+
     @GetMapping("/admin/qlkhachhang")
     public String qlkhachhang() {
         return "/admin/qlkhachhang";
     }
+
     @GetMapping("/admin/addkhachhang")
     public String addkhachhang() {
         return "/admin/addkhachhang";
     }
+
     @GetMapping("/admin/qltheloai")
     public String qltheloai() {
         return "/admin/qltheloai";
     }
+
     @GetMapping("/admin/addtheloai")
     public String addtheloai() {
         return "/admin/addtheloai";
     }
+
     @GetMapping("/admin/qlchitietsanpham")
     public String qlchitietsanpham() {
         return "/admin/qlchitietsanpham";
@@ -64,9 +70,25 @@ public class AdminController {
     public String thuonghieu() {
         return "/admin/addthuonghieu";
     }
+
     @GetMapping("/admin/tab")
     public String tab() {
         return "/admin/tab";
+    }
+
+    @GetMapping("/admin/qldegiay")
+    public String qldegiay() {
+        return "/admin/qldegiay";
+    }
+
+    @GetMapping("/admin/qlchatlieu")
+    public String qlchatlieu() {
+        return "/admin/qlchatlieu";
+    }
+
+    @GetMapping("/admin/qlthuonghieu")
+    public String qlthuonghieu() {
+        return "/admin/qlthuonghieu";
     }
 
 
