59943ee933b63f85a551a6b61048930d1be3cfb9
DQT10503 <128661867+DQT10503@users.noreply.github.com>
2024-03-26 01:15:24 +0700

    ok

--- src/main/java/com/example/demo/controller/khachhang/KhachHangController.java
+++ src/main/java/com/example/demo/controller/khachhang/KhachHangController.java
@@ -1,15 +1,16 @@
 package com.example.demo.controller.khachhang;
 
 import com.example.demo.entity.KhachHang;
+import com.example.demo.entity.NguoiDung;
 import com.example.demo.service.KhachHangService;
 import com.example.demo.service.NguoiDungService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Date;
-import java.util.List;
+import java.io.IOException;
 
 @Controller
 @RequestMapping("khachhang")
@@ -25,7 +26,8 @@ public class KhachHangController {
         model.addAttribute("lstNguoiDung", nguoiDungService.findAll());
         return "admin/qlkhachhang";
     }
-//    @PostMapping("/timkiem")
+
+    //    @PostMapping("/timkiem")
 //    public String search(@RequestParam(required = false) String tenOrSdt,
 //                         @RequestParam(required = false) int trangThai,
 //                         @RequestParam(required = false) Date ngaySinh
@@ -37,4 +39,22 @@ public class KhachHangController {
 //
 //        }
 //    }
+    @GetMapping("/add")
+    public String form(@ModelAttribute("khachhang") NguoiDung nguoiDung
+                       ) {
+        return "admin/addkhachhang";
+    }
+    @PostMapping("/add")
+    public String add(@Valid KhachHang khachHang,
+                      @Valid NguoiDung nguoiDung,
+                      Model model
+                      ) throws IOException {
+//        nguoiDung.setAnh(multipartFile.getOriginalFilename());
+//        multipartFile.transferTo(new File("D:\\DuAnTotNghiep\\DATN\\src\\main\\resources\\static\\img\\khachhang" + multipartFile.getOriginalFilename()));
+//        model.addAttribute("uploadedFile", multipartFile);
+        nguoiDungService.save(nguoiDung);
+        khachHang.setNguoidung(nguoiDung);
+        khachHangService.add(khachHang);
+        return "redirect:/khachhang/add";
+    }
 }
--- src/main/java/com/example/demo/service/KhachHangService.java
+++ src/main/java/com/example/demo/service/KhachHangService.java
@@ -1,9 +1,12 @@
 package com.example.demo.service;
 
 import com.example.demo.entity.KhachHang;
+import com.example.demo.entity.NguoiDung;
 
 import java.util.List;
 
 public interface KhachHangService {
     List<KhachHang> findAll();
+    KhachHang add(KhachHang khachHang);
+
 }
