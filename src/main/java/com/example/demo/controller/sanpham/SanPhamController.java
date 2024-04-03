d385072d47a12d28d7d0a4a43621c49808c4f2ba
Duynvph30146 <duynvph30146@fpt.edu.vn>
2024-04-03 17:51:10 +0700

    oke

--- src/main/java/com/example/demo/controller/sanpham/SanPhamController.java
+++ src/main/java/com/example/demo/controller/sanpham/SanPhamController.java
@@ -15,9 +15,17 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.IOException;
 import java.math.BigDecimal;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.List;
@@ -88,7 +96,7 @@ public class SanPhamController {
     }
 
     //hiển thị các thuộc tính của sản phẩm thông qua modelAttribute
-    @RequestMapping(value = { "/viewaddSP", "/viewaddSP" }, method = { RequestMethod.GET, RequestMethod.POST })
+    @RequestMapping(value = {"/viewaddSP", "/viewaddSP"}, method = {RequestMethod.GET, RequestMethod.POST})
     public String viewaddSP(Model model, @RequestParam(defaultValue = "0") int p,
                             @ModelAttribute("thuonghieu") ThuongHieu thuongHieu,
                             @ModelAttribute("chatlieu") ChatLieu chatLieu,
@@ -112,14 +120,12 @@ public class SanPhamController {
         model.addAttribute("dg", listDeGiay);
         model.addAttribute("cl", listChatLieu);
         model.addAttribute("a", listAnh);
-        Pageable pageable = PageRequest.of(p, 20);
-        Page<SanPhamChiTiet> page = sanPhamChiTietImp.finAllPage(pageable);
-        model.addAttribute("page", page);
         return "admin/addsanpham";
     }
+
     @PostMapping("/addProduct")
-//    @CacheEvict(value = "sanphamCache", allEntries = true)
-    public String addProduct(@RequestParam(defaultValue = "0") int p, Model model, @RequestParam String tensp,
+    public String addProduct(@RequestParam(defaultValue = "0") int p, Model model,
+                             @RequestParam String tensp,
                              @RequestParam String mota,
                              @RequestParam Boolean trangthai,
                              @RequestParam ThuongHieu idThuongHieu,
@@ -128,6 +134,7 @@ public class SanPhamController {
                              @RequestParam(name = "kichCoId") List<String> kichCoNames,
                              @RequestParam DeGiay idDeGiay,
                              @RequestParam List<MauSac> idMauSac
+//                             @RequestParam(name = "anh") List<MultipartFile> anhFiles
     ) {
         SanPham sanPham = new SanPham();
         sanPham.setTensanpham(tensp);
@@ -152,11 +159,18 @@ public class SanPhamController {
                     spct.setKichco(kichCo);
                     spct.setDegiay(idDeGiay);
                     spct.setMausac(colorId);
+                    // Lưu các ảnh vào cơ sở dữ liệu
+//                    for (MultipartFile anhFile : anhFiles) {
+                        // Lưu ảnh vào thư mục trên server và nhận đường dẫn
+//                        String anhUrl = saveImage(anhFile);
+
+                        // Tạo đối tượng Anh và lưu vào sản phẩm chi tiết
+//                        Anh anh = new Anh();
+//                        anh.setTenanh(anhUrl);
+//                        anh.setSanphamchitiet(spct);
+//                        spct.getAnh().add(anh);
+//                    }
                     sanPhamChiTietImp.addSPCT(spct);
-                    Anh anh=new Anh();
-                    anh.setTenanh("https://cdn.tgdd.vn/Products/Images/42/153856/iphone-11-trang-200x200.jpg");
-                    anh.setSanphamchitiet(spct);
-                    anhRepository.save(anh);
                 } else {
                 }
             }
@@ -167,10 +181,42 @@ public class SanPhamController {
     }
 
     @GetMapping("/detailsanpham/{id}")
-    public String detailsanpham(@PathVariable Integer id,Model model) {
-     SanPham sanPham=sanPhamRepositoty.findById(id).orElse(null);
-        model.addAttribute("sanpham",sanPham);
+    public String detailsanpham(@PathVariable Integer id, Model model) {
+        SanPham sanPham = sanPhamRepositoty.findById(id).orElse(null);
+        model.addAttribute("sanpham", sanPham);
         model.addAttribute("sanphamchitiet", sanPham.getSpct());
         return "admin/qlchitietsanpham";
     }
+
+    private String saveImage(MultipartFile file) {
+        // Thư mục để lưu trữ ảnh trên server
+        String uploadDir = "G:\\Ki7\\DATN\\DATN\\src\\main\\resources\\upload";
+
+        try {
+            // Đảm bảo thư mục upload tồn tại
+            File directory = new File(uploadDir);
+            if (!directory.exists()) {
+                directory.mkdirs();
+            }
+
+            // Lấy tên file gốc
+            String originalFileName = file.getOriginalFilename();
+
+            // Tạo đường dẫn đến file trên server
+            String filePath = uploadDir + File.separator + originalFileName;
+
+            // Lưu file vào thư mục trên server
+            File dest = new File(filePath);
+            file.transferTo(dest);
+
+            // Trả về đường dẫn của file
+            return filePath;
+        } catch (IOException e) {
+            e.printStackTrace();
+            // Xử lý khi có lỗi xảy ra
+            return null;
+        }
+    }
+
+
 }
