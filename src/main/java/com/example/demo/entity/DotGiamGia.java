50ea41feb85d4fabc71cccb3d56260a7af2826e7
NHTuanAnh2k2 <nguyenhoangtuananh.na@gmail.com>
2024-04-30 20:43:09 +0700

    dot giam gia tuananh

---
+++ src/main/java/com/example/demo/entity/DotGiamGia.java
@@ -0,0 +1,31 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.sql.Timestamp;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "dotgiamgia")
+public class DotGiamGia {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Integer id;
+    String tendot;
+    Integer giatrigiam;// phần trăm giảm
+    Timestamp ngaybatdau;
+    Timestamp ngayketthuc;
+    Timestamp ngaytao;
+    String nguoitao;
+    Timestamp lancapnhatcuoi;
+    String nguoicapnhat;
+    Integer trangthai;
+}
