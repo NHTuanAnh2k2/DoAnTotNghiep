ca3a9fe188b64b47f3474614d1f5bb03ddd512e1
tungPC <tungdtph30319@fpt.edu.vn>
2024-05-17 15:45:56 +0700

    tung

--- src/main/java/com/example/demo/repository/khachhang/KhachHangRepostory.java
+++ src/main/java/com/example/demo/repository/khachhang/KhachHangRepostory.java
@@ -1,6 +1,7 @@
 package com.example.demo.repository.khachhang;
 
 import com.example.demo.entity.KhachHang;
+import com.example.demo.entity.NhanVien;
 import com.example.demo.info.KhachHangInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -22,4 +23,7 @@ public interface KhachHangRepostory extends JpaRepository<KhachHang, Integer> {
 
     @Query("SELECT k FROM KhachHang k WHERE k.nguoidung.id = ?1")
     KhachHang findByNguoiDung(Integer id);
+
+    @Query("SELECT N FROM KhachHang N WHERE N.nguoidung.hovaten like %?1% or N.nguoidung.sodienthoai like %?1%")
+    List<KhachHang> timNVTheoMa(String ten);
 }
