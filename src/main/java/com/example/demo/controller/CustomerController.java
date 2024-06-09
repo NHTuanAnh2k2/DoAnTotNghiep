f7d9075e34cb2fb787febf74ea7dc63202d9587e
Duynvph30146 <duynvph30146@fpt.edu.vn>
2024-06-09 22:13:06 +0700

    ok

--- src/main/java/com/example/demo/controller/CustomerController.java
+++ src/main/java/com/example/demo/controller/CustomerController.java
@@ -5,7 +5,6 @@ import com.example.demo.entity.NguoiDung;
 import com.example.demo.info.NguoiDungKHInfo;
 import com.example.demo.service.KhachHangService;
 import jakarta.servlet.http.HttpSession;
-import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -16,27 +15,14 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 public class CustomerController {
     @Autowired
     KhachHangService khachHangService;
-    @GetMapping("/trangchu")
-    public String trangchu() {
-        return "customer/trangchu";
-    }
 
     @GetMapping("/modal")
     public String modal() {
         return "customer/modal";
     }
-    @GetMapping("/product/detail")
-    public String productdetail() {
-        return "customer/product-details";
-    }
-    @GetMapping("/cart")
-    public String cart() {
-        return "customer/cart";
-    }
 
     @GetMapping("/quenmatkhau")
-    public String quenmatkhau(@ModelAttribute("nguoidung") NguoiDungKHInfo nguoidung,
-                              Model model) {
+    public String quenmatkhau(@ModelAttribute("nguoidung") NguoiDungKHInfo nguoidung, Model model) {
         return "same/quenmatkhau";
     }
 
@@ -55,8 +41,5 @@ public class CustomerController {
         } else {
             return "redirect:/dangnhap";
         }
-
     }
-
-
 }
